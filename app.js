c07efc95df81dbca4710a6d06c35b201c384384b
MukarramRizvi <imranmaster20@gmail.com>
2024-03-04 10:37:00 +0500

    add two file in project

---
+++ app.js
@@ -0,0 +1,7 @@
+var studentName = [];
+var studentName = ["Mukarram","ALI","rizvi"];
+var string = ["Mukarram","ALI","rizvi"];
+var number = [0,1,2];
+var boolean = [true,false];
+var mixed = ["Mukarram",2,false];
+document.write("<h1>Qualifications:</h1><br>1) SSC<br>2) HSC <br>3) BCS<br>4) BS<br>5) BCOM<br> 6) MS<br>7) M.Phil<br>8) PhD");
